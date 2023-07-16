df98ac0223918ec7da496de492ef920a79a19625
Edwin Balbuena <edwinbalbuena189@gmail.com>
2023-07-16 12:24:31 -0400

    Eliminando la duplicación de las funciones loadEnf y clear
    Clase "RegEnfermedad"

--- ProyectoFinal/src/Visual/RegEnfermedad.java
+++ ProyectoFinal/src/Visual/RegEnfermedad.java
@@ -134,23 +134,7 @@ public class RegEnfermedad extends JFrame {
 		btnCancelar.setBounds(55, 370, 115, 29);
 		contentPane.add(btnCancelar);
 		loadEnf();
-<<<<<<< HEAD
-=======
-	}
-	private void loadEnf() {
-		if(miEnfermedad != null) {
-			textCodigo.setText(miEnfermedad.getCodigo());
-			textNombre.setText(miEnfermedad.getNombre());
-			textAreaDetalles.setText(miEnfermedad.getDescripcion());
-			comboGravedad.setSelectedItem((String) miEnfermedad.getGravedad());
-		}
-	}
-	private void clear() {
-		textCodigo.setText("");
-		textNombre.setText("");
-		textAreaDetalles.setText("");
-		comboGravedad.setSelectedIndex(0);
->>>>>>> branch 'main' of https://github.com/CactuZzz24/-Proyecto-Final-POO-CSTI-1830-4361.git
+
 	}
 	private void loadEnf() {
 		if(miEnfermedad != null) {
@@ -166,4 +150,5 @@ public class RegEnfermedad extends JFrame {
 		textAreaDetalles.setText("");
 		comboGravedad.setSelectedIndex(0);
 	}
+
 }
